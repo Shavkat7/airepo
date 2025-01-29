b896e1946c41358d4325442f3c7874809f59c8cc
Shavkat7 <shavkat.official007@gmail.com>
2025-01-29 21:18:06 +0500

    lesson 15, first 7b tasks

---
+++ lesson-15/homework/all_15_tasks.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# 1. Create a vector with values ranging from 10 to 49.
+
+# vector = np.arange(10, 50)
+# print(vector)
+
+
+# 2. Create a 3x3 matrix with values ranging from 0 to 8.
+
+# matrix = np.arange(9).reshape(3, 3)
+# print(matrix)
+
+# 3. Create a 3x3 identity matrix.
+
+# matrix = np.eye(3, dtype=int)
+# print(matrix)
+
+# 4. Create a 3x3x3 array with random values.
+
+# arr = np.random.randint(0, 100, (3, 3, 3))
+# print(arr)
+
+# 5. Create a 10x10 array with random values and find the minimum and maximum values.
+
+# arr = np.random.randint(0, 100, (10, 10))
+# print(arr, arr.min(), arr.max())
+
+# 6. Create a random vector of size 30 and find the mean value.
+
+# vector = np.random.randint(0, 100, 30)
+# print(vector, vector.mean())
+
+# 7. Normalize a 5x5 random matrix.
+
